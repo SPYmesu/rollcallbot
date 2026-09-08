4144a8068458bc463fec440a7d76b7dcca7bfdd6
SPY_me <admin@spyme.su>
2026-09-08 22:10:08 +0300

    Убрано лишнее поле switchInlineQueryCurrentChat у кнопок

--- src/main/java/su/spyme/rollcallbot/utils/MyUtils.java
+++ src/main/java/su/spyme/rollcallbot/utils/MyUtils.java
@@ -327,7 +327,6 @@ public class MyUtils {
                 .builder()
                 .text(text)
                 .callbackData(callback)
-                .switchInlineQueryCurrentChat(callback)
                 .build();
     }
 
